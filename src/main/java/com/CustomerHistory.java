101cd887f3fbcef176c11112995bbf6599a31cf1
rhdmAdmin <>
2019-04-23 08:54:31 -0400

    {/src/main/java/com/CustomerHistory.java}

--- src/main/java/com/CustomerHistory.java
+++ src/main/java/com/CustomerHistory.java
@@ -9,21 +9,33 @@ public class CustomerHistory implements java.io.Serializable {
 	static final long serialVersionUID = 1L;
 
 	@org.kie.api.definition.type.Label("customerAttribute")
-	private java.lang.Boolean customerAttribute;
+	private String customerAttribute;
+
+	private java.lang.Boolean value;
 
 	public CustomerHistory() {
 	}
 
-	public java.lang.Boolean getCustomerAttribute() {
+	public java.lang.String getCustomerAttribute() {
 		return this.customerAttribute;
 	}
 
-	public void setCustomerAttribute(java.lang.Boolean customerAttribute) {
+	public void setCustomerAttribute(java.lang.String customerAttribute) {
 		this.customerAttribute = customerAttribute;
 	}
 
-	public CustomerHistory(java.lang.Boolean customerAttribute) {
+	public java.lang.Boolean getValue() {
+		return this.value;
+	}
+
+	public void setValue(java.lang.Boolean value) {
+		this.value = value;
+	}
+
+	public CustomerHistory(java.lang.String customerAttribute,
+			java.lang.Boolean value) {
 		this.customerAttribute = customerAttribute;
+		this.value = value;
 	}
 
 }
